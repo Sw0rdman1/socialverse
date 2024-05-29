8f8bc1ae218a70ab5f27c93ad43109afb9b000a6
Bozidar Vujasinovic <bozidar.vujasinovic@scripttic.com>
2024-05-29 21:32:48 +0200

    Update user profile picture URL

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -6,7 +6,7 @@ const user1: User = {
     id: '1',
     email: 'vujasinovicb2019@gmail.com',
     displayName: 'vujasinovicb',
-    profilePicture: "https://wallpapers.com/images/hd/cool-profile-picture-minion-13pu7815v42uvrsg.jpg",
+    profilePicture: 'https://jmrmolshsmmyxcivsxxv.supabase.co/storage/v1/object/sign/avatars/WhatsApp%20Image%202024-02-07%20at%2000.19.24.jpeg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJhdmF0YXJzL1doYXRzQXBwIEltYWdlIDIwMjQtMDItMDcgYXQgMDAuMTkuMjQuanBlZyIsImlhdCI6MTcxNjA1Nzg3NSwiZXhwIjoxNzQ3NTkzODc1fQ.Y65iuGQe9k9ViPuvr40NKa3_i2FKniNGVSLsz3_4RSE&t=2024-05-18T18%3A44%3A35.973Z',
     isFollowing: true,
     numberOfFollowers: 615,
     numberOfFollowing: 423,
