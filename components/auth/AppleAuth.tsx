a20a2e72387aa5e6f9a6bf9b6e9f0a74fc360132
Bozidar Vujasinovic <bozidar.vujasinovic@scripttic.com>
2024-07-08 16:51:01 +0200

    Refactor AppleAuth button style based on theme

--- components/auth/AppleAuth.tsx
+++ components/auth/AppleAuth.tsx
@@ -1,15 +1,17 @@
 import { useAuth } from '@/context/AppContext'
+import { isLightTheme } from '@/hooks/useColors'
 import * as AppleAuthentication from 'expo-apple-authentication'
 import { View } from 'react-native'
 
 export function AppleAuth() {
     const { signInWithApple } = useAuth()
+    const isLight = isLightTheme()
 
     return (
         <View style={{ width: '100%', paddingHorizontal: 30 }}>
             <AppleAuthentication.AppleAuthenticationButton
                 buttonType={AppleAuthentication.AppleAuthenticationButtonType.SIGN_IN}
-                buttonStyle={AppleAuthentication.AppleAuthenticationButtonStyle.BLACK}
+                buttonStyle={isLight ? AppleAuthentication.AppleAuthenticationButtonStyle.BLACK : AppleAuthentication.AppleAuthenticationButtonStyle.WHITE}
                 cornerRadius={10}
                 style={{ width: "100%", height: 50 }}
                 onPress={signInWithApple}
